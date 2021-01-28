7e8726e3a528ff37684dac1bb3bc4df535ee4fbe
zwbetz-gh <zwbetz@gmail.com>
2021-01-27 22:41:16 -0600

    rename main file

--- backend/src/main/java/com/example/fullstacktaskapp/FullstackTaskApp.java
+++ backend/src/main/java/com/example/fullstacktaskapp/FullstackTaskApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Backend {
+public class FullstackTaskApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Backend.class, args);
+		SpringApplication.run(FullstackTaskApp.class, args);
 	}
 
 }
