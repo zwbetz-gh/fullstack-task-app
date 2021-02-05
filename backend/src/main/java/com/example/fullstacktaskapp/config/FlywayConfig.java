818b3f79bc1e6136fc0e08fe638862011eee9572
zwbetz-gh <zwbetz@gmail.com>
2021-02-04 20:08:10 -0600

    control via CUSTOM_FLYWAY_CLEAN, CUSTOM_FLYWAY_MIGRATE

--- backend/src/main/java/com/example/fullstacktaskapp/config/FlywayConfig.java
+++ backend/src/main/java/com/example/fullstacktaskapp/config/FlywayConfig.java
@@ -1,42 +1,37 @@
 package com.example.fullstacktaskapp.config;
 
-import java.util.Arrays;
-import java.util.List;
-
 import lombok.extern.log4j.Log4j2;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Log4j2
 @Configuration
 public class FlywayConfig {
 
-  @Autowired
-  private Environment environment;
+  @Value("${custom.flyway.clean}")
+  private boolean customFlywayClean;
 
-  private static final String CLEAN_DB = "clean-db";
-  private static final String MIGRATE_DB = "migrate-db";
+  @Value("${custom.flyway.migrate}")
+  private boolean customFlywayMigrate;
 
   @Bean
   public FlywayMigrationStrategy flywayMigrationStrategy() {
-    final List<String> activeProfiles = Arrays.asList(environment.getActiveProfiles());
     return flyway -> {
-      if (activeProfiles.contains(CLEAN_DB)) {
-        log.info("{} profile is active. Database will be cleaned", CLEAN_DB);
+      if (customFlywayClean) {
+        log.info("Enabling database clean");
         flyway.clean();
       } else {
-        log.info("{} profile is not active. Database will not be cleaned", CLEAN_DB);
+        log.info("Skipping database clean");
       }
 
-      if (activeProfiles.contains(MIGRATE_DB)) {
-        log.info("{} profile is active. Database will be migrated", MIGRATE_DB);
+      if (customFlywayMigrate) {
+        log.info("Enabling database migrate");
         flyway.migrate();
       } else {
-        log.info("{} profile is not active. Database will not be migrated", MIGRATE_DB);
+        log.info("Skipping database migrate");
       }
     };
   }
